4ee2891792f420ced32d39935dee5fb547669848
jensrott <jensrott@student.arteveldehs.be>
2018-02-14 11:31:03 +0100

    added firebase config

---
+++ sprapple/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { initializeFirebase, sendData } from './utils/firebaseService';
+
+export default class App extends React.Component {
+
+  componentWillMount() {
+    initializeFirebase();
+    sendData();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ sprapple/utils/firebaseService.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase';
+import firebaseConfig from './firebaseConfig.json';
+
+export function intializeFirebase() {
+    if (!firebaseConfig || !firebaseConfig.apiKey || firebaseConfig.apiKey === '<YOUR-API-KEY>') {
+        throw new Error('Add your own firebaseConfig.json file in the folder /utils/firebaseConfig.json');
+      } else {
+          // Initialize the firebase app
+          firebase.initializeApp(firebaseConfig);
+      }
+}
+
+export function sendData() {
+  let defaultFirebase = firebase.database();
+  let test = defaultFirebase.ref('test');
+
+  test.on('child_added', function(snapshot) {
+    console.log(snapshot);
+  });
+
+}
