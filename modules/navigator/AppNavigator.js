41278bfa9be01d7b5803dba70ea7eb8cb00d1fad
jensrott <jensrott@student.arteveldehs.be>
2018-02-15 14:55:20 +0100

    added components, screens

--- modules/navigator/AppNavigator.js
+++ modules/navigator/AppNavigator.js
@@ -3,7 +3,7 @@ import DetailsScreen from '../screens/DetailsScreen';
 import LoginScreen from '../screens/LoginScreen';
 import FeedScreen from '../screens/FeedScreen';
 import ProfileScreen from '../screens/ProfileScreen';
-import AchievementScreen from '../screens/AchievementScreen';
+// import AchievementScreen from '../screens/AchievementScreen';
 import { StackNavigator } from 'react-navigation';
 
 const RootStack = StackNavigator(
@@ -13,7 +13,7 @@ const RootStack = StackNavigator(
     Login: { screen: LoginScreen, },
     Feed: { screen: FeedScreen, },
     Profile: { screen: ProfileScreen, },
-    Achievements: { screen: AchievementScreen, }
+   // Achievements: { screen: AchievementScreen, }
   },
   {
     initialRouteName: 'Home',
